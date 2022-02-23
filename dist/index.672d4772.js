fe953cd40a2a0bd7a6037528b6347d4d457c0991
RathiAnkxrx <ankurrathi15@gmail.com>
2022-02-24 00:19:46 +0530

    fixed untracked files

--- dist/index.672d4772.js
+++
@@ -1,8 +0,0 @@
-const btnNav = document.querySelector(".btn-mobile-nav");
-const header = document.querySelector(".navigation");
-btnNav.addEventListener("click", function() {
-    header.classList.toggle("nav-open");
-});
-console.log("hello");
-
-//# sourceMappingURL=index.672d4772.js.map
