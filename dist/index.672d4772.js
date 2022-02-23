893004cfa1b5f02520ed12cc52a988a41154c867
RathiAnkxrx <ankurrathi15@gmail.com>
2022-02-24 00:07:39 +0530

    fixed gitignore

---
+++ dist/index.672d4772.js
@@ -0,0 +1,8 @@
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".navigation");
+btnNav.addEventListener("click", function() {
+    header.classList.toggle("nav-open");
+});
+console.log("hello");
+
+//# sourceMappingURL=index.672d4772.js.map
